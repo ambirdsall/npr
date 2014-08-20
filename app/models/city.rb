116683a2802179691dd4d3b340b769c047d56c38
Alex Birdsall <ambirdsall@gmail.com>
2014-08-19 17:16:11 -0700

    Wraps station-finding HTTParty get call in City class method

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,9 +1,17 @@
 class City
+  
   attr_reader :city, :state, :stations
   def initialize(city, state)
     @city = city
     @state = state
     @stations = []
+  end
+
+  def find_stations
+    response = HTTParty.get("http://api.npr.org/stations", query: {city: city, state: state, apiKey: API_KEY})
+    response["stations"]["station"].each do |station|
+      add_station(Station.new(station["callLetters"], station["orgId"]))
+    end
   end
 
   def add_station(station)
