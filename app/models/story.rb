9d5d6da4c52194b85de62e5a0259690d6d11ba5b
Alex Birdsall <ambirdsall@gmail.com>
2014-08-21 16:29:03 -0700

    Adds word_count attribute to Story

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,12 +1,17 @@
 class Story
-  attr_reader :grade_level
+  attr_reader :grade_level, :word_count
 
   def initialize text
     @text = text
-    @grade_level = find_grade_level(@text)
+    @grade_level = find_grade_level
+    @word_count = find_word_count
   end
 
-  def find_grade_level text
-    Lingua::EN::Readability.new(text).kincaid
+  def find_grade_level
+    Lingua::EN::Readability.new(@text).kincaid
+  end
+
+  def find_word_count
+    Lingua::EN::Readability.new(@text).num_words
   end
 end
