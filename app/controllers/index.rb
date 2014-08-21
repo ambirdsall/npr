89c633a0b515b850ab449dbcf6419d10381787a3
Alex Birdsall <ambirdsall@gmail.com>
2014-08-21 12:34:25 -0700

    Adds error for cities with stations but no text

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,10 +6,14 @@ post '/city' do
   @user_city = City.new(params[:city], params[:state])
   begin
     @user_city.find_stations 
-    @user_city.stations.each do |station|
-      station.find_stories
+    @user_city.find_stories
+
+    if @user_city.has_stories?
+      erb :graphs, layout: false
+    else
+      @error = "This city's stations don't publish text stories. <br>Try again or turn on the radio."
+      erb :error, layout: false
     end
-  erb :graphs, layout: false
   rescue
     @error = "Try another city: we couldn't find that one."
     erb :error, layout: false
