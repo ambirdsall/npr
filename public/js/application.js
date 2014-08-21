a748e29aa07d61d2d7d3a54fff461073a5816fcf
Alex Birdsall <ambirdsall@gmail.com>
2014-08-20 20:36:10 -0700

    minor fixes to test graph; adds pseudocode in application.js comments

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,13 @@
-var searchterms = polyjs.data({
+var stationGrades = polyjs.data({
   gradeLevel: [9.34130177514793,8.859733038348086,10.488697788697792,6.92278643476012,8.69156479731149,10.048655937364071,5.697836585365852,11.110889464594127,5.2882139194139235,9.611282506785098]
 //     term: ["Flower"], "Chocolate", "Candy", "Valentine"],
 //     count: [16600000, 9140000, 7480000, 5000000]
 });
-var chart_holder = document.getElementsByClass("chart");
+var chart_holder = document.getElementById("chart");
 
 var sfc = polyjs.chart({
     layer: {
-        data: searchterms,
+        data: stationGrades,
         type: "point",
         y: "gradeLevel",
         opacity: { const: 0.5 },
@@ -31,3 +31,13 @@ var sfc = polyjs.chart({
     height: 350
 });
 // store station name and grade array as JSON in data attribute
+
+// modify below function to find station data divs
+// function ReplaceContentInContainer(selector, content) {
+//   var nodeList = document.querySelectorAll(selector);
+//   for (var i = 0, length = nodeList.length; i < length; i++) {
+//      nodeList[i].innerHTML = content;
+//   }
+// }
+
+// ReplaceContentInContainer(".theclass", "HELLO WORLD");
