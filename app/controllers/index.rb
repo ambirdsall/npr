cb2be4e3ce45ca584b205e8a26946abd291e9c92
Alex Birdsall <ambirdsall@gmail.com>
2014-08-21 11:40:28 -0700

    Errors raised by querying nonexistant city routed to error partial

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,10 +4,13 @@ end
 
 post '/city' do
   @user_city = City.new(params[:city], params[:state])
-  @user_city.find_stations
-  @user_city.stations.each do |station|
-    station.find_stories
-  end
-
+  begin
+    @user_city.find_stations 
+    @user_city.stations.each do |station|
+      station.find_stories
+    end
   erb :graphs, layout: false
+  rescue
+    erb :error, layout: false
+  end
 end
