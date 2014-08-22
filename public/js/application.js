360cca86e0ad354bf2ccdd2f26225a76a5eab53f
Alex Birdsall <ambirdsall@gmail.com>
2014-08-21 19:54:33 -0700

    Maps word count to point color

--- public/js/application.js
+++ public/js/application.js
@@ -4,6 +4,7 @@ var graphGrades = function(stationName, stationGrades, chartDiv) {
           data: stationGrades,
           type: "point",
           y: "gradeLevel",
+          color: "wordCount",
           opacity: { const: 0.5 },
           size: { const: 3 }
       },
@@ -16,6 +17,16 @@ var graphGrades = function(stationName, stationGrades, chartDiv) {
           title: "Grade Level",
           min: 0,
           max: 20
+        },
+        color: {
+          scale: {
+            type: 'gradient2',
+            lower: '#339966',
+            middle: '#666666',
+            upper: '#6600cc',
+            midpoint: 500
+          },
+          title: "Words"
         }
       },
 
@@ -40,7 +51,8 @@ var graphStations = function() {
     if (thereAreStoriesFor(stationList[i])) {
       var callLetters = stationList[i].dataset.name;
       var gradeLevels = polyjs.data({
-        gradeLevel: JSON.parse(stationList[i].dataset.grades)
+        gradeLevel: JSON.parse(stationList[i].dataset.grades),
+        wordCount: JSON.parse(stationList[i].dataset.wordCounts)
       });
 
       graphGrades(callLetters, gradeLevels, stationList[i]);
